05427d80002139eb1f8ae651b71597ee8cfbfcc3
Vaibhav Mishra <mishravaibhav033@gmail.com>
2022-09-29 21:41:19 +0530

    updates

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
           img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/11/06/FNK_Gulab-Jamun_H1.jpg.rend.hgtvcom.826.620.suffix/1604695335894.jpeg",
           description:
             "Boil sugar and water to prepare syrup remove from fire; add cardamom and keep aside to cool.In a mixing bowl put the flour, paneer, sooji, Nestlé MILKMAID , baking powder and baking soda. Mix gently to make a soft dough, do not knead too much.Divide the mixture into 30-35 portions and gently roll into round gulab jamuns. Fry in the oil at very low flame till golden brown in color.Keep putting the fried gulab jamuns in the prepared sugar syrup, once all the gulab jamuns are in the sugar syrup bring it to a boil and remove from flame.Serve warm.",
-          showRecipe: true,
+          showRecipe: false,
         },
         {
           id: "2",
